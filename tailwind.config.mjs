17b0d45f9b1981e14086986f025a3dca083be28e
Dominik Sieroń <dominiksieron13@gmail.com>
2024-02-17 15:41:56 +0100

    Changes: config, add colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+// @import url('https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap')
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'main-purple': '#533C96',
+				'secondary-purple': '#762CDF',
+				'supplement-purple': '#726BD9',
+				'background': '#08010E',
+				'negation': '#EBDAF8',
+				'underlight': '#B6ABD7',
+				'hover': '#A27FFF',
+				'block': '#1B1223',
+				'inblock': '#30253C'
+			},
+			fontFamily: {
+				'plus-jakarta-sans': ['Plus Jakarta Sans', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
 }
